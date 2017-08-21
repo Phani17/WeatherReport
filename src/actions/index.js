2dbf6d13fc632c6898070c9dce39a08887b0fbbd
Phani17 <phanindrasarma20@gmail.com>
2017-08-21 09:55:28 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,6 @@ const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}
 
 export const FETCH_WEATHER ='FETCH_WEATHER'
 
-//always have to return an action
 export function fetchWeather(city){
   const url= `${ROOT_URL}&q=${city},in`
   const request =axios.get(url)
