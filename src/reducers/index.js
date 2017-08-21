2cecfc4eacf5e9ba82cd552e544430f191abcef5
Phani17 <phanindrasarma20@gmail.com>
2017-08-21 09:58:36 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import WeatherReducer from './weather_reducer'
 
 const rootReducer = combineReducers({
-  //state: (state = {}) => state
   weather:WeatherReducer
 });
 
